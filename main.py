db14c0d0410793db9e1235e72f3e2f6082042829
Julien Raynal <julien.raynal@epsi.fr>
2023-12-03 15:46:31 +0100

    added the graph plotting

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import numpy as np
 import networkx as nx
 from typing import List
 import matplotlib.pyplot as plt
+from matplotlib import colors as mcolors
+
 
 def mystery_algorithm(L: List[int]) -> int:
     """
@@ -25,7 +27,7 @@ def mystery_algorithm(L: List[int]) -> int:
     T: np.ndarray = np.zeros(n + 2, dtype=int)  # Borned signed permutations
     P: np.ndarray = np.zeros(n + 2, dtype=int)  # Reference to the values position in the T table
     C: np.ndarray = np.full(2 * n + 2, np.nan)  # Table of cycles
-    i: int  
+    i: int
     j: int
     k: int
 
@@ -61,19 +63,21 @@ def mystery_algorithm(L: List[int]) -> int:
         C[i] = cpt  # Tag the cycle
         b = False  # While False we're in the same cycle
         while b is False:
-            i = i + 1 if (i % 2) == 0 else i - 1  # Follows an edge of reality, either left (if the value is odds) or right (if the value is even)
+            i = i + 1 if (
+                                 i % 2) == 0 else i - 1  # Follows an edge of reality, either left (if the value is odds) or right (if the value is even)
             C[i] = cpt  # Tags the index with the current cycle
             # Follows the edge of desir (determines the value of j which is the next index of the edge of desir)
-            if (i % 2) == 0: 
+            if (i % 2) == 0:
                 k = abs(T[i // 2])  # The absolut value allows to search in the P table the position of T
-                if T[i // 2] > 0:  # Allows to know which to next number we are, depending if it's even or odds we're either at the beginning or the end   
+                if T[
+                    i // 2] > 0:  # Allows to know which to next number we are, depending if it's even or odds we're either at the beginning or the end
                     j = 2 * P[k + 1] - 1 if T[P[k + 1]] >= 0 else 2 * P[k + 1]
                 else:
-                     j = 2 * P[k - 1] if T[P[k - 1]] >= 0 else 2 * P[k - 1] - 1  # Permet d'obtenir la fin d'un élément
+                    j = 2 * P[k - 1] if T[P[k - 1]] >= 0 else 2 * P[k - 1] - 1  # Permet d'obtenir la fin d'un élément
             else:
                 k = abs(T[(i + 1) // 2])
                 if (T[(i + 1) // 2]) > 0:
-                    j = 2 * P[k - 1] if T[P[k - 1]] >= 0 else 2 * P[k - 1] - 1 
+                    j = 2 * P[k - 1] if T[P[k - 1]] >= 0 else 2 * P[k - 1] - 1
                 else:
                     j = 2 * P[k + 1] - 1 if T[P[k + 1]] >= 0 else 2 * P[k + 1]
 
@@ -90,9 +94,12 @@ def mystery_algorithm(L: List[int]) -> int:
 
     return cpt
 
-def draw_graph(T: List[int], P: List[int], C: List[int]) -> None:
+
+def draw_graph(T: np.ndarray, P: np.ndarray, C: np.ndarray) -> None:
     """
     Draw the breakpoint graph
+    The edges of reality are in blue and the edges of desire other colors (one color per cycle)
+    The names of the nodes is formated as: b(eginning)/e(nd) + node number
 
     Parameters
     ----------
@@ -100,25 +107,82 @@ def draw_graph(T: List[int], P: List[int], C: List[int]) -> None:
     P: Ordered values of T in a list
     C: Half points of T and the cycle it belongs to
     """
+    colors = list(mcolors.BASE_COLORS)
+    ax = plt.gca()
     g = nx.Graph()
+    #############################
+    #        Adding nodes       #
+    #############################
     nodes: list = []
+    level = 0  # Used to align the nodes
     for idx, t in enumerate(T):
-        nodes.append((t, {"level": idx}))
+        # checking if it's a borning number
+        if t == 0:
+            nodes.append((f"e{t}", {"level": level, "beginning": False}))
+            level += 1
+        elif t == len(T) - 1:
+            nodes.append((f"b{t}", {"level": level, "beginning": True}))
+        else:
+            if t > 0:
+                nodes.append((f'b{t}', {"level": level, "beginning": True}))
+                level += 1
+                nodes.append((f'e{t}', {"level": level, "beginning": False}))
+                level += 1
+            else:
+                nodes.append((f'e{t}', {"level": level, "beginning": False}))
+                level += 1
+                nodes.append((f'b{t}', {"level": level, "beginning": True}))
+                level += 1
+        # nodes.append((t, {"level": idx}))
 
-    g.add_edges_from(nodes)
-
-    # Adding edges of reality
-    for i in range(0, len(T), 2):
-        print(g.nodes[T[i]])
-        #g.add_edge(g.nodes[T[i]], g.nodes[T[i + 1]])
-
+    # g.add_edges_from(nodes)
+    g.add_nodes_from(nodes)
     pos = nx.multipartite_layout(g, subset_key="level")
-    nx.draw(g, pos, with_labels=True)
+    nodes = list(g)
+
+    #############################
+    #       edges of reality    #
+    #############################
+    for i in range(0, len(nodes) - 1, 2):
+        g.add_edge(nodes[i], nodes[i + 1], reality=True)
+    nx.draw_networkx_edges(g, pos, edgelist=g.edges(), width=1, edge_color=colors[0])
+    g.add_edge(nodes[0], nodes[4], reality=False)
+
+    #############################
+    #       edges of desir      #
+    #############################
+    beginning_nodes: list = [b for (b, i) in g.nodes(data=True) if i["beginning"] is True]
+    ending_nodes: list = [b for (b, i) in g.nodes(data=True) if i["beginning"] is False]
+    desired_edges: list = []
+    # Adding to the graph the edges of desir
+    for i in range(len(P) - 1):  # Using P as it contains the ordered positions of T, the nodes used to create the nodes list
+        ending_node = ending_nodes[P[i]]
+        beginning_node = beginning_nodes[P[i + 1] - 1]
+        cycle = int(C[nodes.index(ending_node)])
+        desired_edges.append((ending_node, beginning_node, cycle))
+    # Adding the curved edges
+    for edge in desired_edges:
+        end, beginning, cycle = edge
+        rad = 0.5
+        arrowprops = dict(arrowstyle='-',
+                          color=colors[cycle + 1],
+                          connectionstyle=f"arc3,rad={rad}",
+                          linestyle='-',
+                          alpha=0.6, )
+        ax.annotate("",
+                    xy=pos[end],
+                    xytext=pos[beginning],
+                    arrowprops=arrowprops)
+
+    nx.draw_networkx_nodes(g, pos, node_size=200)
+    nx.draw_networkx_labels(g, pos, font_size=10, font_family="sans-serif")
+
+    plt.box(False)
     plt.show()
 
 
 if __name__ == '__main__':
     L: List[int] = [-2, -1, 3]
-    #L: List[int] = [3, -4, -2, -1]
+    # L: List[int] = [3, -4, -2, -1]
     cpt = mystery_algorithm(L)
     print(cpt)
