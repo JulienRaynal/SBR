c3abbe77174205871011028fa85571e9fef1d04e
Julien Raynal <julien.raynal@epsi.fr>
2023-12-03 17:45:33 +0100

    both graph added

--- main.py
+++ main.py
@@ -63,8 +63,7 @@ def mystery_algorithm(L: List[int]) -> int:
         C[i] = cpt  # Tag the cycle
         b = False  # While False we're in the same cycle
         while b is False:
-            i = i + 1 if (
-                                 i % 2) == 0 else i - 1  # Follows an edge of reality, either left (if the value is odds) or right (if the value is even)
+            i = i + 1 if (i % 2) == 0 else i - 1  # Follows an edge of reality, either left (if the value is odds) or right (if the value is even)
             C[i] = cpt  # Tags the index with the current cycle
             # Follows the edge of desir (determines the value of j which is the next index of the edge of desir)
             if (i % 2) == 0:
@@ -90,12 +89,13 @@ def mystery_algorithm(L: List[int]) -> int:
 
     print(f"T:\t{T}\nP:\t{P}\nC:\t{C.astype(int)}")
     print(f"The number of inversion needed to order correctly is: {len(L) + 1 - cpt}")
-    draw_graph(T, P, C)
+    desired_edges = draw_breakpoint_graph(T, P, C)
+    draw_overlap_graph(desired_edges, P)
 
     return cpt
 
 
-def draw_graph(T: np.ndarray, P: np.ndarray, C: np.ndarray) -> None:
+def draw_breakpoint_graph(T: np.ndarray, P: np.ndarray, C: np.ndarray) -> list:
     """
     Draw the breakpoint graph
     The edges of reality are in blue and the edges of desire other colors (one color per cycle)
@@ -118,24 +118,22 @@ def draw_graph(T: np.ndarray, P: np.ndarray, C: np.ndarray) -> None:
     for idx, t in enumerate(T):
         # checking if it's a borning number
         if t == 0:
-            nodes.append((f"e{t}", {"level": level, "beginning": False}))
+            nodes.append((f"e{t}", {"level": level, "overlap_level": idx, "beginning": False}))
             level += 1
         elif t == len(T) - 1:
-            nodes.append((f"b{t}", {"level": level, "beginning": True}))
+            nodes.append((f"b{t}", {"level": level, "overlap_level": idx - 1, "beginning": True}))
+        # checking if the number is oriented or not and assigning the right value in each case
         else:
             if t > 0:
-                nodes.append((f'b{t}', {"level": level, "beginning": True}))
+                nodes.append((f'b{t}', {"level": level, "overlap_level": idx - 1, "beginning": True}))
                 level += 1
-                nodes.append((f'e{t}', {"level": level, "beginning": False}))
+                nodes.append((f'e{t}', {"level": level, "overlap_level": idx, "beginning": False}))
                 level += 1
             else:
-                nodes.append((f'e{t}', {"level": level, "beginning": False}))
+                nodes.append((f'e{t}', {"level": level, "overlap_level": idx, "beginning": False}))
                 level += 1
-                nodes.append((f'b{t}', {"level": level, "beginning": True}))
+                nodes.append((f'b{t}', {"level": level, "overlap_level": idx - 1, "beginning": True}))
                 level += 1
-        # nodes.append((t, {"level": idx}))
-
-    # g.add_edges_from(nodes)
     g.add_nodes_from(nodes)
     pos = nx.multipartite_layout(g, subset_key="level")
     nodes = list(g)
@@ -143,10 +141,10 @@ def draw_graph(T: np.ndarray, P: np.ndarray, C: np.ndarray) -> None:
     #############################
     #       edges of reality    #
     #############################
+    reality_edges: list = []
     for i in range(0, len(nodes) - 1, 2):
-        g.add_edge(nodes[i], nodes[i + 1], reality=True)
-    nx.draw_networkx_edges(g, pos, edgelist=g.edges(), width=1, edge_color=colors[0])
-    g.add_edge(nodes[0], nodes[4], reality=False)
+        reality_edges.append((nodes[i], nodes[i + 1]))
+    nx.draw_networkx_edges(g, pos, edgelist=reality_edges, width=1, edge_color=colors[0])
 
     #############################
     #       edges of desir      #
@@ -160,7 +158,7 @@ def draw_graph(T: np.ndarray, P: np.ndarray, C: np.ndarray) -> None:
         beginning_node = beginning_nodes[P[i + 1] - 1]
         cycle = int(C[nodes.index(ending_node)])
         desired_edges.append((ending_node, beginning_node, cycle))
-    # Adding the curved edges
+    # Adding the curved edges (didn't find another way to add straight and curved edges)
     for edge in desired_edges:
         end, beginning, cycle = edge
         rad = 0.5
@@ -174,11 +172,51 @@ def draw_graph(T: np.ndarray, P: np.ndarray, C: np.ndarray) -> None:
                     xytext=pos[beginning],
                     arrowprops=arrowprops)
 
+    #############################
+    #  Plotting edges and nodes #
+    #############################
     nx.draw_networkx_nodes(g, pos, node_size=200)
     nx.draw_networkx_labels(g, pos, font_size=10, font_family="sans-serif")
-
     plt.box(False)
     plt.show()
+    return desired_edges
+
+
+def draw_overlap_graph(desired_edges, P: np.ndarray) -> None:
+    g1 = nx.Graph()
+    #############################
+    #        Adding nodes       #
+    #############################
+    level = 0  # Used to align the nodes
+    for edge in desired_edges:
+        ending_node = int(edge[0][1:])
+        beginning_node = int(edge[1][1:])
+        x_ending = P[abs(ending_node)] * 2 if ending_node >= 0 else P[abs(ending_node)] * 2 - 1 - 1
+        x_beginning = P[abs(beginning_node)] * 2 - 1 - 1 if beginning_node > 0 else P[abs(beginning_node)] * 2
+        g1.add_node(edge[0], pos=(x_ending, -level), cycle=edge[2])
+        g1.add_node(edge[1], pos=(x_beginning, -level), cycle=edge[2])
+        level+=1
+
+    pos = nx.get_node_attributes(g1, 'pos')
+
+    #############################
+    #        joining nodes      #
+    #############################
+    nodes = g1.nodes(data=True)
+    dict_nodes = {}
+    for node in nodes:
+        node_position = node[1]["pos"][0]
+        if not dict_nodes.get(node_position):
+            dict_nodes[node_position] = (node[0],)
+        else:
+            dict_nodes[node_position] += (node[0],)
+    nx.draw_networkx_nodes(g1, pos, node_size=200)
+    nx.draw_networkx_labels(g1, pos, font_size=10, font_family="sans-serif")
+    nx.draw_networkx_edges(g1, pos, edgelist=desired_edges, width=1, edge_color="red")
+    nx.draw_networkx_edges(g1, pos, edgelist=list(dict_nodes.values()), width=1)
+    plt.box(False)
+    plt.show()
+
 
 
 if __name__ == '__main__':
